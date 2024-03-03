964c940e0661d0e8ec53d4d84fda73bdd8db6535
brianmina17 <brian.mina17@gmail.com>
2024-03-03 10:37:46 -0500

    cleaning the conversaton file using re funtion

--- pythonProject1/bot.py
+++ pythonProject1/bot.py
@@ -2,9 +2,15 @@
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from cleaner import clean_corpus
+
+CORPUS_FILE = "chat_brother.txt"
 
 chatbot = ChatBot("Chatpot")
 trainer = ListTrainer(chatbot)
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+
+trainer.train(cleaned_corpus)
 trainer.train([
     "Hi",
     "Welcome, friend 🤗",
